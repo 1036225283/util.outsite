ff2959f067e1367ebe48f9c4c0d1f3ed1a79fe7b
admin <id512@126.com>
2015-05-04 14:05:56 +0800

    添加验证手机号和获取字符编码

---
+++ util/src/main/java/com/nitian/util/verify/UtilVerify.java
@@ -0,0 +1,70 @@
+package com.nitian.util.verify;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UtilVerify {
+
+	/**
+	 * 验证手机号
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static boolean isMobile(String str) {
+		Pattern p = null;
+		Matcher m = null;
+		boolean b = false;
+		p = Pattern.compile("^[1][3,4,5,8][0-9]{9}$"); // 验证手机号
+		m = p.matcher(str);
+		b = m.matches();
+		return b;
+	}
+
+	/**
+	 * 获取字符编码
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static String getEncoding(String str) {
+		String encode = "GB2312";
+		try {
+			if (str.equals(new String(str.getBytes(encode), encode))) {
+				String s = encode;
+				return s;
+			}
+		} catch (Exception exception) {
+		}
+		encode = "ISO-8859-1";
+		try {
+			if (str.equals(new String(str.getBytes(encode), encode))) {
+				String s1 = encode;
+				return s1;
+			}
+		} catch (Exception exception1) {
+		}
+		encode = "UTF-8";
+		try {
+			if (str.equals(new String(str.getBytes(encode), encode))) {
+				String s2 = encode;
+				return s2;
+			}
+		} catch (Exception exception2) {
+		}
+		encode = "GBK";
+		try {
+			if (str.equals(new String(str.getBytes(encode), encode))) {
+				String s3 = encode;
+				return s3;
+			}
+		} catch (Exception exception3) {
+		}
+		return "";
+	}
+
+	public static void main(String[] args) {
+		// System.out.println(UtilVerify.isMobile("13607144955"));
+		System.out.println(UtilVerify.getEncoding("ddddd"));
+	}
+}
