ee774f70e3e49c1a50df8f17e2f68db8e2b70422
admin <id512@126.com>
2015-05-23 14:47:41 +0800

    添加创建spring-mvc hibernate dao service 代码

---
+++ util/src/main/java/code/spring_mvc_hibernate/CreateCode.java
@@ -0,0 +1,60 @@
+package code.spring_mvc_hibernate;
+
+import com.nitian.util.file.UtilFile;
+import com.nitian.util.string.UtilString;
+
+public class CreateCode {
+
+	private String entityName;
+
+	private String outPath;
+
+	public static void main(String[] args) {
+		CreateCode code = new CreateCode("Test",
+				"F:\\spring-mvc\\spring-mvc\\src\\main\\java\\project\\");
+		code.write();
+	}
+
+	public CreateCode(String entityName, String outPath) {
+		// TODO Auto-generated constructor stub
+		this.entityName = entityName;
+		this.outPath = outPath;
+	}
+
+	public void write() {
+		String daoPath = (CreateCode.class.getResource("").toString() + "template/daoTemplate.tlt")
+				.replace("file:/", "");
+		String servicePath = (CreateCode.class.getResource("").toString() + "template/serviceTemplate.tlt")
+				.replace("file:/", "");
+
+		String daoOutPath = outPath + entityName + "Dao.java";
+		String serviceOutPath = outPath + entityName + "Service.java";
+
+		String daoString = UtilFile.fileToString(daoPath);
+		String serviceString = UtilFile.fileToString(servicePath);
+
+		daoString = daoString.replace("#EntityName#", entityName);
+		serviceString = serviceString.replace("#EntityName#", entityName);
+		String lowerEntityName = UtilString.letterLower(entityName);
+		serviceString = serviceString.replace("#entityName#", lowerEntityName);
+
+		UtilFile.stringToFile(daoString, daoOutPath);
+		UtilFile.stringToFile(serviceString, serviceOutPath);
+	}
+
+	public String getEntityName() {
+		return entityName;
+	}
+
+	public void setEntityName(String entityName) {
+		this.entityName = entityName;
+	}
+
+	public String getOutPath() {
+		return outPath;
+	}
+
+	public void setOutPath(String outPath) {
+		this.outPath = outPath;
+	}
+}
