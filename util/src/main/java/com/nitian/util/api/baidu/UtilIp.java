745183d48ee66142d3f1e2c8f6117581a1076636
xws <id512@126.com>
2015-09-10 16:23:56 +0800

    根据ip提出地理位置信息，从地理位置信息中提取省份和城市

--- util/src/main/java/com/nitian/util/api/baidu/UtilIp.java
+++ util/src/main/java/com/nitian/util/api/baidu/UtilIp.java
@@ -1,7 +1,5 @@
 package com.nitian.util.api.baidu;
 
-import net.sf.json.JSONObject;
-
 import org.apache.commons.lang.StringEscapeUtils;
 
 import com.nitian.util.http.UtilHttp;
@@ -9,27 +7,27 @@ import com.nitian.util.json.JsonStringToObject;
 
 public class UtilIp {
 
-	public static String url = "http://api.map.baidu.com/location/ip?ak="+Ak.ak;
-	
-	public static String getLocation(String ip){
-		url = url+"&coor=bd09ll";
-		url = url+"&ip="+ip;
+	private static String url = "http://api.map.baidu.com/location/ip?ak="
+			+ Ak.ak;
+
+	public static String getLocation(String ip) {
+		url = url + "&coor=bd09ll";
+		url = url + "&ip=" + ip;
 		String result = UtilHttp.get(url);
 		result = StringEscapeUtils.unescapeJava(result);
 		return result;
 	}
-	
+
 	public static void main(String[] args) {
 		String s = getLocation("223.99.217.3");
 		JsonStringToObject jsonStringToObject = new JsonStringToObject();
 		jsonStringToObject.goString(s);
-		String json =  jsonStringToObject.get("content.address_detail.city");
-		 System.out.println(json);
-//		JSONObject json = JSONObject.fromObject(s);
-		;
-//		System.out.println(json.get("[1]"));
+		String city = jsonStringToObject.get("content.address_detail.city");
+		String province = jsonStringToObject
+				.get("content.address_detail.province");
+		System.out.println(province + " " + city);
 		System.out.println(s);
-//		String a = UtilGeocoding.geocoderByLocation("36.10521490,120.38442818");
-//		System.out.println(a);
+		// UtilGeocoding.geocoderByLocation("36.10521490,120.38442818");
+		// System.out.println(a);
 	}
 }
