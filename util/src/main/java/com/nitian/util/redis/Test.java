1fbfca2bcec38e6977d1190dc9d81eb4a1bed375
xws <id512@126.com>
2016-03-08 15:40:16 +0800

    添加redis的哈希操作

--- util/src/main/java/com/nitian/util/redis/Test.java
+++ util/src/main/java/com/nitian/util/redis/Test.java
@@ -1,9 +1,21 @@
 package com.nitian.util.redis;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Test {
 
 	public static void main(String[] args) {
 		UtilRedisString.put("user", "xws");
 		System.out.println(UtilRedisString.get("user"));
+		
+		
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("username", "xws");
+		map.put("password", "xws123445");
+		map.put("age", "24");
+		UtilRedisHash.put("user1", map);
+		System.out.println(UtilRedisHash.get("user1"));
+		
 	}
 }
--- util/src/main/java/com/nitian/util/redis/UtilRedisHash.java
+++ util/src/main/java/com/nitian/util/redis/UtilRedisHash.java
@@ -1,51 +1,36 @@
 package com.nitian.util.redis;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import redis.clients.jedis.Jedis;
 
 public class UtilRedisHash {
 
-	private String mapName;
-
 	public static Jedis jedis = new Jedis("localhost");
 
-	public UtilRedisHash(String mapName) {
-		// TODO Auto-generated constructor stub
-		this.mapName = mapName;
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("id", "id");
-		jedis.hmset(mapName, map);
+	/**
+	 * 获取值
+	 * 
+	 * @param key
+	 * @return
+	 */
+	public static Map<String, String> get(String key) {
+		Map<String, String> map = jedis.hgetAll(key);
+		return map;
 	}
 
 	/**
-	 * 删除map的值key
+	 * 存放值
 	 * 
 	 * @param key
+	 * @param map
 	 */
-	public void remove(String key) {
-		jedis.hdel(mapName, key);
-	}
-
-	public String get(String key) {
-		List<String> list = jedis.hmget(mapName, key);
-		if (list.isEmpty()) {
-			return null;
-		} else {
-			return list.get(0);
-		}
-	}
-
-	/**
-	 * 存放key-value
-	 * 
-	 * @param key
-	 * @param value
-	 */
-	public void put(String key, String value) {
-		jedis.hset(mapName, key, value);
+	public static void put(String key, Map<String, String> map) {
+		jedis.hmset(key, map);
+		// for (Map.Entry<String, String> entry : map.entrySet()) {
+		// jedis.hset(key, entry.getKey(), entry.getValue());
+		//
+		// }
 	}
 
 }
